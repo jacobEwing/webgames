22b433641bc77d8a616ea372100c0325d6f397bb
Jacob Ewing <jacob@kvack.org>
2015-05-06 00:15:15 -0400

    Updating Swix with newer code
    Starting a new copy of Swix to incorporate my sprite library and use updated
    sprites to improve the animation.  Will attempt to update the general code
    style as well.

---
+++ swix_new/swix.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Coming+Soon' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="swix.css" />
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="spriteSet.js"></script>
+	<script type="text/javascript">
+		var cellSprite, cell;
+		$().ready(function(){
+			cellSprite = new spriteSet('tiles.sprite', function(){
+				cell = new spriteClass(cellSprite);
+				//cell.drawFrame($('#spriteTest'), 'blue', 50, 50);
+				cell.setFrame('gold');
+				cell.position(100, 100);
+				cell.draw($('#spriteTest'));
+				cell.startSequence('rotgold');
+			});
+
+		});
+	</script>
+</head>
+<body>
+	<div id="gameWrapper">
+		<div id="spriteTest" style="border: 1px dashed #FFF; border-radius: 10px; width: 600px; height: 400px; color: #FFF; margin: 1em">
+			<?php
+				$sequences = array(
+					'blue2black', 'black2blue', 'gold2black', 'black2gold', 'rotblue', 'rotgold', 'rotblack'
+				);
+				foreach($sequences as $sequence){
+					echo '<br/><a href="#" onclick="cell.startSequence(\'' . $sequence . '\'); return false;">' . $sequence . '</a>';
+				}
+			?>
+		</div>
+		<div id="centering">
+			<div id="loadingPrompt"></div>
+			<div id="contentWrapper"><!-- style="display:none"-->
+				<div id="swix"><img src="images/swix.png"/></div>
+				<img id="copyright" src="images/copyright.png"/>
+				<img id="stepstitle" src="images/moves.png"/>
+				<div id="stepstaken"></div>
+				<img id="beststepstitle" src="images/best.png"/>
+				<div id="beststeps"></div>
+				<div id="content"></div>
+				<img id="resetButton" class="anchor" src="images/reset.png" onclick="restartLevel()"/>
+				<img id="exitButton" class="anchor" src="images/exit.png" onclick="exit()"/>
+				<img id="skipButton" class="anchor" src="images/skip.png" onclick="skipLevel()"/>
+				<div id="titleDivWrapper">
+					<div id="titleDiv"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
