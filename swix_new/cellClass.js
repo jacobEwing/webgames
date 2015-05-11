4b5f71da0e9b5defad79a70b1ca71a62ae7cf067
Jacob Ewing <jacob@kvack.org>
2015-05-10 20:05:51 -0400

    Integrated new swix sprite into old code
    Very basic integration.  Game is roughly playable, but several UI elements need
    to be fixed, and a couple of bugs have shown their faces already.

---
+++ swix_new/cellClass.js
@@ -0,0 +1,278 @@
+var constants = function(){
+	this.rotationAng = Math.PI / 36; // five degrees
+	return {
+		'rotcos' : Math.cos(this.rotationAng),
+		'rotsin' : Math.sin(this.rotationAng)
+	};
+}();
+
+var globals = {
+	'animating' : 0
+};
+
+var cellClass = function(){
+	this.sprite = this.active = this.celltype = null;
+	this.position = {x : 0, y : 0};
+	this.children = [];
+	if(arguments.length == 1) this.initialize(arguments[0]);
+};
+
+cellClass.prototype.initialize = function(params){
+	//var me = this;
+	for(var idx in params){
+		switch(idx){
+			case 'sprite':
+				this.sprite = new spriteClass(params[idx]);
+				break;
+			case 'active': case 'celltype':
+				this[idx] = params[idx];
+				break;
+			case 'position':
+				try{
+					this.position = {
+						'x' : params[idx].x,
+						'y' : params[idx].y
+					};
+				}catch(e){
+					throw "cellClass::initialize: position parameter expects an object with x, y values";
+				}
+				break;
+			default:
+				throw "cellClass::initialize: Invalid parameter \"" + idx + "\"";
+		}
+	}
+	if(this.sprite == null || this.active == null || this.celltype == null){
+		throw "cellClass::initialize: parameters must include 'sprite', 'active', and 'celltype'";
+	}
+
+	this.sprite.position(this.position.x, this.position.y);
+
+	if(this.active){
+		if(this.celltype == 'flip'){
+			this.sprite.setFrame('blue');
+		}else if(this.celltype == 'spin'){
+			this.sprite.setFrame('gold');
+		}
+	}else{
+		this.sprite.setFrame('black');
+	}
+	this.sprite.image.data('cell', this);
+	this.sprite.image.click(
+		function(){
+			var cell = $(this).data('cell');
+			cell.act();
+			return false;
+		}
+	);
+	this.sprite.image.mousedown(function(){return false;});
+};
+
+cellClass.prototype.draw = function(target){
+	var realpos = realPosition(this.position.x, this.position.y);
+	this.sprite.position(realpos.x, realpos.y);
+	this.sprite.draw(target);
+};
+
+cellClass.prototype.act = function(){
+	if(!this.active || globals.animating) return;
+	if(this.celltype == 'spin'){
+		this.rotNeighbours();
+	}else if(this.celltype == 'flip'){
+		this.flipNeighbours();
+	}
+};
+
+cellClass.prototype.getNeighbours = function(){
+	var n;
+	var rval = [];
+	for(n = 0; n < cells.length; n++){
+		if(cells[n].position.y == this.position.y){
+			if(Math.abs(cells[n].position.x - this.position.x) == 1){
+				rval[rval.length] = cells[n];
+			}
+		}else if(cells[n].position.y == this.position.y - 1){
+			dx = cells[n].position.x - this.position.x;
+			if(dx == 1 || dx == 0){
+				rval[rval.length] = cells[n];
+			}
+		}else if(cells[n].position.y == this.position.y + 1){
+			dx = cells[n].position.x - this.position.x;
+			if(dx == -1 || dx == 0){
+				rval[rval.length] = cells[n];
+			}
+		}
+	}
+	return rval;
+};
+
+// flips the six cells that surround the current selected one
+cellClass.prototype.flipNeighbours = function(){
+	var n;
+	var neighbourList = this.getNeighbours();
+	for(n = 0; n < neighbourList.length; n++){
+		neighbourList[n].flip();
+	}
+};
+
+cellClass.prototype.flip = function(){
+	globals.animating++;
+	var flipParams = {
+		'callback' : function(){
+			globals.animating--;
+			if(globals.animating == 0){
+				checkForWin();
+			}
+		}
+	};
+
+	if(this.celltype == 'flip'){
+		if(this.active){
+			this.sprite.startSequence('blue2black', flipParams);
+			this.active = 0;
+		}else{
+			this.sprite.startSequence('black2blue', flipParams);
+			this.active = 1;
+		}
+	}else if(this.celltype == 'spin'){
+		if(this.active){
+			this.sprite.startSequence('gold2black', flipParams);
+			this.active = 0;
+		}else{
+			this.sprite.startSequence('black2gold', flipParams);
+			this.active = 1;
+		}
+	}
+};
+
+cellClass.prototype.setPosition = function(x, y, noDraw){
+	if(noDraw == undefined) noDraw = false;
+	this.position.x = x;
+	this.position.y = y;
+	pos = this.realPosition(x, y);
+	if(!noDraw){
+		this.sprite.setPosition(pos.x, pos.y);
+	}
+};
+
+cellClass.prototype.rotNeighbours = function(){
+	// first, grab the neighbours we'll be rotating
+	var n;
+	var dx, dy;
+	var me = this;
+	var childSequence;
+	var myRealPos = this.realPosition();
+	this.children = this.getNeighbours();
+	for(n = 0; n < this.children.length; n++){
+		//this.children[n].sprite.element.remove().appendTo($('#spriteTest'));
+		this.children[n].sprite.element.appendTo($('#spriteTest'));
+		var childRealPos = this.children[n].realPosition();
+		this.children[n].transformation = {
+			sine : Math.sin(5),
+			cosine : Math.cos(5),
+			relPos : {
+				x : childRealPos.x - myRealPos.x,
+				y : childRealPos.y - myRealPos.y
+			}
+		};
+//		$('body').append(this.children[n].transformation.relPos.x + ', ' + this.children[n].transformation.relPos.y);
+		if(this.children[n].active){
+			if(this.children[n].celltype == 'flip'){
+				childSequence = 'rotblue';
+			}else if(this.children[n].celltype == 'spin'){
+				childSequence = 'rotgold';
+			}else{
+				// shouldn't happen, but for the sake of completeness
+				throw "cellClass::rotNeighbours: Invalid cell type";
+			}
+		}else{
+			childSequence = 'rotblack';
+		}
+		globals.animating++;
+
+		this.children[n].sprite.startSequence(childSequence, {'method' : 'manual'});
+
+		dx = this.children[n].position.x - this.position.x;
+		dy = this.children[n].position.y - this.position.y;
+
+		if(dy == 0){
+			if(dx == 1){
+				this.children[n].setPosition(this.position.x, this.position.y + 1, true);
+			}else if(dx == -1){
+				this.children[n].setPosition(this.position.x, this.position.y - 1, true);
+			}
+		}else if(dy == -1){
+			if(dx == 1){
+				this.children[n].setPosition(this.position.x + 1, this.position.y, true);
+			}else if(dx == 0){
+				this.children[n].setPosition(this.position.x + 1, this.position.y - 1, true);
+			}
+		}else if(dy == 1){
+			if(dx == -1){
+				this.children[n].setPosition(this.position.x - 1, this.position.y, true);
+			}else if(dx == 0){
+				this.children[n].setPosition(this.position.x - 1, this.position.y + 1, true);
+			}
+		}
+	}
+
+	this.sprite.startSequence('rotgold', {
+		'stepCallback' : function(currentFrame){
+			//alert(currentFrame);
+			var myRealPos = me.realPosition();	
+			var n, newx, newy;
+			for(n = 0; n < me.children.length; n++){
+				newx = me.children[n].transformation.relPos.x * constants.rotcos - me.children[n].transformation.relPos.y * constants.rotsin;
+				newy = me.children[n].transformation.relPos.x * constants.rotsin + me.children[n].transformation.relPos.y * constants.rotcos;
+				me.children[n].transformation.relPos = { x : newx, y : newy };
+
+				me.children[n].sprite.position(
+					newx + myRealPos.x + 32,
+					newy + myRealPos.y + 32 
+				);
+
+				me.children[n].sprite.doSequenceStep();
+			}
+			
+		},
+		'callback' : function(){
+			var n;
+			for(n = 0; n < me.children.length; n++){
+				globals.animating--;
+				me.children[n].transformation = undefined;
+
+			}
+		}
+	});
+
+};
+
+cellClass.prototype.realPosition = function(){
+	return realPosition(this.position.x, this.position.y);
+};
+
+function realPosition(x, y){
+	return {'x':48 * x - drawOffset.x, 'y': 27.5 * x + 55 * y + drawOffset.y};
+}
+
+//var cellSprite, cells;
+//var drawOffset = {x : 0, y : 0};
+
+/*
+$().ready(function(){
+
+	cells = [];
+	cellSprite = new spriteSet('tiles.sprite', function(){
+		var n;
+		for(n = 0; n < 9; n++){
+			cells[n] = new cellClass({
+				'sprite' : cellSprite,
+				'active' : 1,
+				'celltype' : n % 2 ? 'flip' : 'spin',
+				'position' : {x : 3 + (n % 3), y : Math.floor(n / 3) - (n % 3 == 2 ? 1 : 0)}
+			});
+			cells[n].draw($('#spriteTest'));
+		}
+	});
+
+});
+*/
--- swix_new/swix.js
+++ swix_new/swix.js
@@ -1,254 +1,405 @@
-var constants = function(){
-	this.rotationAng = Math.PI / 36; // five degrees
-	return {
-		'rotcos' : Math.cos(this.rotationAng),
-		'rotsin' : Math.sin(this.rotationAng)
+var drawOffset, animationTally;
+var offTally;
+var gameState, currentLevel = 0;
+var cells;
+var stepCounter, bestCounter;
+var levelmap, testlevel;
+var levelQueue;
+var hint;
+var cellSprite;
+
+levelQueue = [];
+
+var digiType = function(objName){
+	var image;
+	var position;
+	var canvas;
+	var digit;
+	var value;
+
+	this.image = {
+		0:null, //new imgElement('images/digits/0.png'),
+		1:null, //new imgElement('images/digits/1.png'),
+		2:null, //new imgElement('images/digits/2.png'),
+		3:null, //new imgElement('images/digits/3.png'),
+		4:null, //new imgElement('images/digits/4.png'),
+		5:null, //new imgElement('images/digits/5.png'),
+		6:null, //new imgElement('images/digits/6.png'),
+		7:null, //new imgElement('images/digits/7.png'),
+		8:null, //new imgElement('images/digits/8.png'),
+		9:null, //new imgElement('images/digits/9.png')
 	};
-}();
+	this.position = {'x':0, 'y':0};
+	this.canvas = null;
+	this.digit = [];
+	this.value = 0;
 
-var cellClass = function(){
-	this.sprite = this.active = this.celltype = null;
-	this.position = {x : 0, y : 0};
-	this.children = [];
-	if(arguments.length == 1) this.initialize(arguments[0]);
-};
-
-cellClass.prototype.initialize = function(params){
-	//var me = this;
-	for(var idx in params){
-		switch(idx){
-			case 'sprite':
-				this.sprite = new spriteClass(params[idx]);
-				break;
-			case 'active': case 'celltype':
-				this[idx] = params[idx];
-				break;
-			case 'position':
-				try{
-					this.position = {
-						'x' : params[idx].x,
-						'y' : params[idx].y
-					};
-				}catch(e){
-					throw "cellClass::initialize: position parameter expects an object with x, y values";
-				}
-				break;
-			default:
-				throw "cellClass::initialize: Invalid parameter \"" + idx + "\"";
-		}
-	}
-	if(this.sprite == null || this.active == null || this.celltype == null){
-		throw "cellClass::initialize: parameters must include 'sprite', 'active', and 'celltype'";
+	this.setCanvas = function(newCanvas){
+		//this.canvas = newCanvas;
 	}
 
-	this.sprite.position(this.position.x, this.position.y);
-
-	if(this.active){
-		if(this.celltype == 'flip'){
-			this.sprite.setFrame('blue');
-		}else if(this.celltype == 'spin'){
-			this.sprite.setFrame('gold');
-		}
-	}else{
-		this.sprite.setFrame('black');
-	}
-	this.sprite.image.data('cell', this);
-	this.sprite.image.click(
-		function(){
-			var cell = $(this).data('cell');
-			cell.act();
-		}
-	);
-};
-
-cellClass.prototype.draw = function(target){
-	var realpos = realPosition(this.position.x, this.position.y);
-	this.sprite.position(realpos.x, realpos.y);
-	this.sprite.draw(target);
-};
-
-cellClass.prototype.act = function(){
-	if(!this.active) return;
-	if(this.celltype == 'spin'){
-		this.rotNeighbours();
-	}else if(this.celltype == 'flip'){
-		this.flipNeighbours();
-	}
-};
-
-cellClass.prototype.getNeighbours = function(){
-	var n;
-	var rval = [];
-	for(n = 0; n < cells.length; n++){
-		if(cells[n].position.y == this.position.y){
-			if(Math.abs(cells[n].position.x - this.position.x) == 1){
-				rval[rval.length] = cells[n];
-			}
-		}else if(cells[n].position.y == this.position.y - 1){
-			dx = cells[n].position.x - this.position.x;
-			if(dx == 1 || dx == 0){
-				rval[rval.length] = cells[n];
-			}
-		}else if(cells[n].position.y == this.position.y + 1){
-			dx = cells[n].position.x - this.position.x;
-			if(dx == -1 || dx == 0){
-				rval[rval.length] = cells[n];
-			}
-		}
-	}
-	return rval;
-};
-
-// flips the six cells that surround the current selected one
-cellClass.prototype.flipNeighbours = function(){
-	var n;
-	var neighbourList = this.getNeighbours();
-	for(n = 0; n < neighbourList.length; n++){
-		neighbourList[n].flip();
-	}
-};
-
-cellClass.prototype.flip = function(){
-	if(this.celltype == 'flip'){
-		if(this.active){
-			this.sprite.startSequence('blue2black');
-			this.active = 0;
-		}else{
-			this.sprite.startSequence('black2blue');
-			this.active = 1;
-		}
-	}else if(this.celltype == 'spin'){
-		if(this.active){
-			this.sprite.startSequence('gold2black');
-			this.active = 0;
-		}else{
-			this.sprite.startSequence('black2gold');
-			this.active = 1;
-		}
-	}
-};
-
-cellClass.prototype.setPosition = function(x, y, noDraw){
-	if(noDraw == undefined) noDraw = false;
-	this.position.x = x;
-	this.position.y = y;
-	pos = this.realPosition(x, y);
-	if(!noDraw){
-		this.sprite.setPosition(pos.x, pos.y);
-	}
-};
-
-cellClass.prototype.rotNeighbours = function(){
-	// first, grab the neighbours we'll be rotating
-	var n;
-	var dx, dy;
-	var me = this;
-	var childSequence;
-	var myRealPos = this.realPosition();
-	this.children = this.getNeighbours();
-	for(n = 0; n < this.children.length; n++){
-		var childRealPos = this.children[n].realPosition();
-		this.children[n].transformation = {
-			sine : Math.sin(5),
-			cosine : Math.cos(5),
-			relPos : {
-				x : childRealPos.x - myRealPos.x,
-				y : childRealPos.y - myRealPos.y
-			}
-		};
-//		$('body').append(this.children[n].transformation.relPos.x + ', ' + this.children[n].transformation.relPos.y);
-		if(this.children[n].active){
-			if(this.children[n].celltype == 'flip'){
-				childSequence = 'rotblue';
-			}else if(this.children[n].celltype == 'spin'){
-				childSequence = 'rotgold';
-			}else{
-				// shouldn't happen, but for the sake of completeness
-				throw "cellClass::rotNeighbours: Invalid cell type";
-			}
-		}else{
-			childSequence = 'rotblack';
-		}
-		this.children[n].sprite.startSequence(childSequence, {'method' : 'manual'});
-
-		dx = this.children[n].position.x - this.position.x;
-		dy = this.children[n].position.y - this.position.y;
-
-		if(dy == 0){
-			if(dx == 1){
-				this.children[n].setPosition(this.position.x, this.position.y + 1, true);
-			}else if(dx == -1){
-				this.children[n].setPosition(this.position.x, this.position.y - 1, true);
-			}
-		}else if(dy == -1){
-			if(dx == 1){
-				this.children[n].setPosition(this.position.x + 1, this.position.y, true);
-			}else if(dx == 0){
-				this.children[n].setPosition(this.position.x + 1, this.position.y - 1, true);
-			}
-		}else if(dy == 1){
-			if(dx == -1){
-				this.children[n].setPosition(this.position.x - 1, this.position.y, true);
-			}else if(dx == 0){
-				this.children[n].setPosition(this.position.x - 1, this.position.y + 1, true);
-			}
-		}
+	this.reset = function(newvalue){
+		/*
+		if(newvalue == undefined) newvalue = 0;
+		this.value = newvalue;
+		this.draw();
+		*/
 	}
 
-	this.sprite.startSequence('rotgold', {
-		'stepCallback' : function(currentFrame){
-			//alert(currentFrame);
-			var myRealPos = me.realPosition();	
-			var n, newx, newy;
-			for(n = 0; n < me.children.length; n++){
-				newx = me.children[n].transformation.relPos.x * constants.rotcos - me.children[n].transformation.relPos.y * constants.rotsin;
-				newy = me.children[n].transformation.relPos.x * constants.rotsin + me.children[n].transformation.relPos.y * constants.rotcos;
-				me.children[n].transformation.relPos = { x : newx, y : newy };
+	this.increment = function(){
+		/*
+		this.value++;
+		this.draw();
+		*/
+	}
 
-				me.children[n].sprite.position(
-					newx + myRealPos.x,
-					newy + myRealPos.y
-				);
-
-				me.children[n].sprite.doSequenceStep();
-			}
-			
-		},
-		'callback' : function(){
-			var n;
-			for(n = 0; n < me.children.length; n++){
-				me.children[n].transformation = undefined;
-
+	this.eraseDigits = function(){
+		/*
+		for(n in this.digit){
+			if(this.digit[n] != undefined){
+				this.digit[n].element.remove();
+				this.digit[n] = undefined;
 			}
 		}
-	});
+		*/
+	}
 
-};
+	this.draw = function(numval){
+	/*
+		this.eraseDigits();
+		
+		if(numval == undefined) numval = this.value;
+		numval = Math.floor(numval);
+		numtext = '' + numval;
+		for(n = 0; n < numtext.length; n++){
+			this.drawDigit(numtext.substr(n, 1), n);
+		}
+	*/	
+	}
 
-cellClass.prototype.realPosition = function(){
-	return realPosition(this.position.x, this.position.y);
-};
-
-function realPosition(x, y){
-	return {'x':48 * x - drawOffset.x, 'y': 27.5 * x + 55 * y + drawOffset.y};
+	this.drawDigit = function(value, column){	
+	/*
+		if(this.canvas == null) return false;
+		if(this.digit[column] != undefined){
+			this.digit[column].element.remove();
+			this.digit[column] = undefined;
+		}
+		this.digit[column] = this.image[1 * value].copy();
+		this.digit[column].appendTo(this.canvas);
+		this.digit[column].setPosition(10 * column, 6 * column);
+	*/
+	}
 }
 
-var cellSprite, cells;
-var drawOffset = {x : 0, y : 0};
-$().ready(function(){
+function incAnimationTally(increment){
+	animationTally += increment;
+	if(animationTally == 0){
+		if(offTally == 0){
+			gameState = 'won';
+			setTimeout('finishLevel()', 600);
+		}
+	}
+}
 
-	cells = [];
-	cellSprite = new spriteSet('tiles.sprite', function(){
-		var n;
-		for(n = 0; n < 9; n++){
-			cells[n] = new cellClass({
-				'sprite' : cellSprite,
-				'active' : 1,
-				'celltype' : n % 2 ? 'flip' : 'spin',
-				'position' : {x : 3 + (n % 3), y : Math.floor(n / 3) - (n % 3 == 2 ? 1 : 0)}
+function checkForWin(){
+	var n;
+	for(n = 0; n < cells.length && cells[n].active; n++);
+	if(n >= cells.length){
+		// all cells are active!
+			gameState = 'won';
+			setTimeout('finishLevel()', 600);
+	}
+}	
+
+function finishLevel(){
+	currentLevel++;
+	clearLevel(function(){getLevel(currentLevel)});
+}
+
+function getLevel(levelNum){
+	gameState = 'loading';
+	if(levelQueue[levelNum] == undefined){
+		loadLevel(levelNum, function(){getLevel(levelNum);});
+	}else{
+		levelMap = levelQueue[levelNum];
+		if(levelMap['cells'] != undefined){
+			drawLevel(function(){
+				showTitle(levelMap['title']);
 			});
-			cells[n].draw($('#spriteTest'));
+
+			// go ahead and cache the next level
+			if(1 * levelNum == levelNum){
+				nextLevelnum = 1 * levelNum + 1;
+				if(levelQueue[nextLevelnum] == undefined){
+					setTimeout('loadLevel(' + nextLevelnum + ')', 1000);
+				}
+			}
+		}else{
+			showHint(levelMap['message'], function(){currentLevel = 0; getLevel(currentLevel);}, true);
+		}
+
+	}
+}
+
+function loadLevel(levelNum, callback){
+
+	$.get('swix.php', {'action':'getLevel', 'id':levelNum}, function(resultstring){
+		eval('levelQueue["' + levelNum + '"] = ' + resultstring);
+		if(callback != undefined) callback();
+	});
+}
+
+function showTitle(title){
+	var titleDiv = $('#titleDiv');
+	titleDiv.html(title);
+	titleDiv.fadeIn('medium').delay(500).fadeOut('medium');
+}
+
+function clearLevel(callback){
+	gameState = 'clearing';
+	var n;
+	if(callback == undefined) callback = function(){};
+	animationTally = cells.length;
+	for(n = 0; n < cells.length; n++){
+		cells[n].sprite.element.animate({'top':'+=' + $(window).height() + 'px'}, Math.random() * 500 + 300, 'swing', function(){
+			animationTally --;
+			if(animationTally == 0){
+				finishClearLevel(callback);
+			}
+		});
+	}
+}
+
+function finishClearLevel(callback){
+	animationTally = 0;
+	stepCounter.reset();
+	offTally = 0;
+	drawOffset = {x:0, y:0};
+	cells = [];
+	$('#content').empty();
+	if(callback != undefined) callback()
+}
+
+var disableHints;
+disableHints = false;
+
+function drawLevel(callback){
+	var x, y, numCells = 0;
+	if(callback == undefined) callback = function(){};
+	drawOffset = {'x':levelMap['offset'][0], 'y':levelMap['offset'][1]};
+	$('#titlebar').html(levelMap['title']);
+	animationTally = 0;
+	bestCounter.value = levelMap['best'];
+	bestCounter.draw();
+	testLevel = levelMap['testlevel'] != undefined;
+	offTally = testLevel ? 9999 : 0;
+	for(celltype in levelMap['cells']){
+		plateSet = levelMap['cells'][celltype];
+
+		// can't just say "plateSet.length" when using the custom
+		// json_encode function, so we'll calculate it's length manually.
+		var objlen = 0;
+		for(elem in plateSet) objlen++;
+
+		for(n = 0; n < objlen; n += 3){
+			animationTally++;
+			cells[numCells] = new cellClass({
+				'sprite' : cellSprite,
+				'celltype' : celltype,
+				'active' : plateSet[n + 2],
+				'position' : { x : plateSet[n], y : plateSet[n + 1]}
+
+			});
+			offTally += (1 - plateSet[n + 2]);
+			var realPos = cells[numCells].realPosition();
+			cells[numCells].sprite.element.css({'top':'-100px', 'left' : realPos.x + 'px'});
+			cells[numCells].sprite.element.animate({'top': + realPos.y + 'px'}, Math.random() * 500 + 300, 'swing', function(){
+				animationTally--;
+				if(animationTally == 0){
+					gameState = 'playing';
+					if(levelMap['hint'] != undefined && !disableHints){
+						showHint(levelMap['hint'], callback);
+						levelMap['hint'] = undefined; // we only want the hint once per loading of the level
+					}else{
+						callback();
+					}
+				}
+			});
+			cells[numCells].sprite.appendTo($('#content'));
+			numCells++;
+		}
+	}
+
+}
+
+function closeHint(){
+	colourBack.fadeTo(400, 0);
+	backing.fadeTo(400, 0, function(){
+		backing.remove();
+		colourBackHolder.remove();
+		if(hintCallback != undefined){
+			hintCallback();
 		}
 	});
+}
 
-});
+function toggleHints(){
+	disableHints = !disableHints;
+}
+
+function showHint(promptText, callback, hideDisable){
+	if(hideDisable == undefined) hideDisable = false;
+	if(callback == undefined){
+		hintCallback = function(){};
+	}else{
+		hintCallback = callback;
+	}
+
+	colourBackHolder = $('<div></div>');
+	colourBackHolder.css({"width":"0px", "height":"0px", "top":"0px", "left":"0px", "position":"fixed"});
+	colourBack = $('<div></div>');
+	colourBack.css({'position':'absolute', 'top':'0', 'left':'0', 'background-color':'#000'});
+	colourBack.appendTo(colourBackHolder);	
+	colourBack.css({'width':$(document).width() + 'px', 'height':$(document).height() + 'px'});
+	colourBack.fadeTo(0, 0);
+
+	backing = $('<div></div>');
+	backing.addClass('screenOverlay');
+	backing.fadeTo(0, 0);
+
+	wrapper = $('<div></div>');
+	wrapper.addClass('promptWrapper');
+
+	hint = $('<div></div>');
+	hint.addClass('promptBox');
+
+	hint.append(promptText);
+	footbit = $('<div></div>');
+	footbit.css({'clear':'both', 'padding-top':'2em', 'position':'relative'});
+	if(!hideDisable){
+		checkbox = $('<div></div>');
+		checkbox.addClass('disableHints');
+		checkText = '<input type="checkbox" id="disable" class="customcheck" onclick = "toggleHints();"';
+		if(disableHints) checkText += ' SELECTED';
+		checkText += '> Disable Tips</div>';
+		checkbox.append(checkText);
+
+		footbit.append(checkbox);
+	}
+	okButton = $('<input type="button" value="Close" class="promptButton"></input>');
+	okButton.appendTo(footbit);
+	okButton.click(function(){closeHint();});
+	footbit.appendTo(hint);
+
+	hint.appendTo(wrapper);
+	wrapper.appendTo(backing);
+
+	colourBackHolder.appendTo($('body'));
+	backing.appendTo($('body'));
+	wrapper.css({'top': (backing.height() - wrapper.height()) / 2 + 'px'});
+
+
+	// all assembled - let's fade in
+	colourBack.fadeTo(400, 0.6);
+	backing.fadeTo(400, 0.9);
+}
+
+function restartLevel(){
+	if(gameState == 'playing'){
+		clearLevel(function(){drawLevel()});
+	}
+}
+
+function skipLevel(){
+	if(gameState == 'playing'){
+		currentLevel++;
+		clearLevel(function(){getLevel(currentLevel)});
+	}
+}
+var lodingTally;
+var loadingAnim;
+function startGame(step){
+	if(step == undefined){
+		step = 'init';
+	}
+	switch(step){
+		case 'init':
+			gameState = 'initializing';
+			stepCounter = new digiType();
+			stepCounter.setCanvas($('#stepstaken'));
+			bestCounter = new digiType();
+			bestCounter.setCanvas($('#beststeps'));
+			startGame('loadbg');
+			break;
+		case 'loadbg':
+			$('<img src="images/backdrop2.jpg">').load(function(){
+				startGame('loadLogo');
+			});
+			break;
+		case 'loadLogo':
+			$('<img src="images/swix.png">').load(function(){
+				startGame('loadAnimation');
+			});
+			break;
+		case 'loadAnimation':
+			$('#loadingPrompt').append('<img src="images/swix.png"/><br/><span id="loadingText">Loading</span>');
+			loadingAnim = setInterval(function(){
+				var ang = Math.random() * 2 * Math.PI;
+				var xoffset = 2 * Math.sin(ang);
+				var yoffset = 2 * Math.cos(ang);
+				c = 160 + Math.floor(Math.random() * 96);
+				$('#loadingText').html(Math.random() < 0.5 ? '10@d1n6' : 'Loading');
+				$('#loadingText').css({
+					'padding-top' : xoffset,
+					'padding-left' : yoffset,
+					'color' : 'rgb(' + c + ', ' + c + ', ' + c + ')',
+					'font-size' : (Math.random() * 2 + 39) + 'px'
+				});
+			}, 20);
+			startGame('cache');
+			break;
+		case 'cache':
+			// cache the images for gameplay
+			var imageList = [
+				'digits/6.png', 'digits/0.png', 'digits/2.png', 'digits/7.png',
+				'digits/5.png', 'digits/9.png', 'digits/3.png', 'digits/8.png', 'digits/4.png',
+				'digits/1.png', 'reset.png', 'exit.png', 'moves.png', 'bigBlueArrow.png',
+				'hexgrid.png', 'best.png', 'skip.png',
+				'bigBlueTile.png', 'copyright.png',
+				'tiles.png'
+			];
+			loadingTally = imageList.length;
+			for(var n in imageList){
+				$('<img src="images/' + imageList[n] + '">').load(function(){ loadingTally--;});
+			}
+			setTimeout(function(){
+				startGame('finishCache');
+			}, 300);
+			break;
+		case 'finishCache':
+			if(loadingTally){
+				setTimeout(function(){
+					startGame('finishCache');
+				}, 300);
+			}else{
+				startGame('fadeIn');
+			}
+			break;
+		case 'fadeIn':
+			$('#loadingPrompt').fadeTo('slow', 0);
+			$('#contentWrapper').fadeTo('slow', 1, function(){
+				clearInterval(loadingAnim);
+				startGame('complete');
+			});
+			break;
+		case 'complete':
+			cellSprite = new spriteSet('tiles.sprite');
+			stepCounter.draw();
+			bestCounter.draw();
+			finishClearLevel();
+			getLevel(currentLevel);
+			break;
+		default:
+			alert('invalid startGame step: "' + step + '"');
+	}
+}
