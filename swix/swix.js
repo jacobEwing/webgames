05964af741b64e24f66eec3ff2a42b44973731ee
Jacob Ewing <jacob@kvack.org>
2020-05-26 18:06:11 -0400

    Added strict syntax and update code to match
    The only issue I ran into was scope problems - global variables declared locally.  Fixed.

--- swix/swix.js
+++ swix/swix.js
@@ -1,3 +1,4 @@
+'use strict';
 var drawOffset, animationTally;
 var offTally;
 var gameState, currentLevel = 0;
@@ -8,6 +9,7 @@ var levelQueue;
 var hint;
 var cellSprite;
 var disableHints = false;
+var levelMap, stepsTaken;
 
 levelQueue = [];
 
@@ -74,7 +76,7 @@ function finishClearLevel(callback){
 
 
 function drawLevel(callback){
-	var x, y, numCells = 0;
+	var x, y, n, numCells = 0, plateSet;
 	if(callback == undefined) callback = function(){};
 	drawOffset = {'x':levelMap['offset'][0], 'y':levelMap['offset'][1]};
 	$('#titlebar').html(levelMap['title']);
@@ -82,13 +84,13 @@ function drawLevel(callback){
 	beststeps = levelMap['best'];
 	$('#beststeps').html(beststeps);
 	offTally = 0;
-	for(celltype in levelMap['cells']){
+	for(var celltype in levelMap['cells']){
 		plateSet = levelMap['cells'][celltype];
 
 		// can't just say "plateSet.length" when using the custom
 		// json_encode function, so we'll calculate it's length manually.
 		var objlen = 0;
-		for(elem in plateSet) objlen++;
+		for(var elem in plateSet) objlen++;
 
 		for(n = 0; n < objlen; n += 3){
 			animationTally++;
@@ -121,16 +123,6 @@ function drawLevel(callback){
 
 }
 
-function closeHint(){
-	colourBack.fadeTo(400, 0);
-	backing.fadeTo(400, 0, function(){
-		backing.remove();
-		colourBackHolder.remove();
-		if(hintCallback != undefined){
-			hintCallback();
-		}
-	});
-}
 
 function toggleHints(){
 	disableHints = !disableHints;
@@ -139,45 +131,54 @@ function toggleHints(){
 function showHint(promptText, callback, hideDisable){
 	if(hideDisable == undefined) hideDisable = false;
 	if(callback == undefined){
-		hintCallback = function(){};
-	}else{
-		hintCallback = callback;
+		callback = function(){};
 	}
-
-	colourBackHolder = $('<div></div>');
+	
+	var colourBackHolder = $('<div></div>');
 	colourBackHolder.css({"width":"0px", "height":"0px", "top":"0px", "left":"0px", "position":"fixed"});
-	colourBack = $('<div></div>');
+	var colourBack = $('<div></div>');
 	colourBack.css({'position':'absolute', 'top':'0', 'left':'0', 'background-color':'#000'});
 	colourBack.appendTo(colourBackHolder);	
 	colourBack.css({'width':$(document).width() + 'px', 'height':$(document).height() + 'px'});
 	colourBack.fadeTo(0, 0);
 
-	backing = $('<div></div>');
+	var closeHint = function (callback){
+		colourBack.fadeTo(400, 0);
+		backing.fadeTo(400, 0, function(){
+			backing.remove();
+			callback();
+		});
+	}
+
+	var backing = $('<div></div>');
 	backing.addClass('screenOverlay');
 	backing.fadeTo(0, 0);
 
-	wrapper = $('<div></div>');
+	var wrapper = $('<div></div>');
 	wrapper.addClass('promptWrapper');
 
-	hint = $('<div></div>');
+	var hint = $('<div></div>');
 	hint.addClass('promptBox');
 
 	hint.append(promptText);
-	footbit = $('<div></div>');
+	var footbit = $('<div></div>');
 	footbit.css({'clear':'both', 'padding-top':'2em', 'position':'relative'});
 	if(!hideDisable){
-		checkbox = $('<div></div>');
+		var checkbox = $('<div></div>');
 		checkbox.addClass('disableHints');
-		checkText = '<input type="checkbox" id="disable" class="customcheck" onclick = "toggleHints();"';
+		var checkText = '<input type="checkbox" id="disable" class="customcheck" onclick = "toggleHints();"';
 		if(disableHints) checkText += ' SELECTED';
 		checkText += '> Disable Tips</div>';
 		checkbox.append(checkText);
 
 		footbit.append(checkbox);
 	}
-	okButton = $('<input type="button" value="Close" class="promptButton"></input>');
+	var okButton = $('<input type="button" value="Close" class="promptButton"></input>');
 	okButton.appendTo(footbit);
-	okButton.click(function(){closeHint();});
+	okButton.click(function(){closeHint(function(){
+		colourBackHolder.remove();
+		callback();
+	});});
 	footbit.appendTo(hint);
 
 	hint.appendTo(wrapper);
@@ -238,7 +239,7 @@ var startGame = function(){
 					var ang = Math.random() * 2 * Math.PI;
 					var xoffset = 2 * Math.sin(ang);
 					var yoffset = 2 * Math.cos(ang);
-					c = 160 + Math.floor(Math.random() * 96);
+					var c = 160 + Math.floor(Math.random() * 96);
 					$('#loadingText').html(Math.random() < 0.5 ? '10@d1n6' : 'Loading');
 					$('#loadingText').css({
 						'font-family' : 'Loved by the King',
